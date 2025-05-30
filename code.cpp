26833b42ee426fe4592fe9c9d5728379542d6eff
logicadellee <adelleee1@gmail.com>
2025-05-30 11:10:43 +0700

    tambahfilecppdanfondasiawal

---
+++ code.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+// tipedata_1
+struct Pasien {
+    string nama;
+    long long nik;
+    string gejala;
+    string waktuDaftar;
+};
+
+int main() {
+
+    return 0;
+}
