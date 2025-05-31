cc1d908f19cd16acff545768a0a21bf9c620ded8
logicadellee <adelleee1@gmail.com>
2025-05-31 22:26:15 +0700

    Tambah data dan buat fungsi tambah pasien

--- code.cpp
+++ code.cpp
@@ -3,11 +3,27 @@ using namespace std;
 
 // tipedata_1
 struct Pasien {
+    int nomorAntrian;
     string nama;
     long long nik;
-    string gejala;
+    int usia;
+    string Keluhan;
     string waktuDaftar;
+    int prioritas;
 };
+
+// Menambahkan Pasien
+void tambahPasien(string nama, long long nik, int usia, string keluhan,string waktuDaftar,int prioritas) {
+    pasien p = {nomorAntrian++,nama, nik, usia, keluhan, waktuDaftar, prioritas};
+    antrian.push(p);
+    semuaPasien.pushBack(p);
+    cout << "Pasien " << nama << " Berhasil ditambahkan ke antrian.\n";
+}
+
+// Menampilkan Antrian
+void tampilkanAntrian() {
+    
+}
 
 int main() {
 
